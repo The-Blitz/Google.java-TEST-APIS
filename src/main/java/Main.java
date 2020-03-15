353f2a10bf0fe7904ae7be3c4870b778e50677d3
The-Blitz <a20112387@pucp.pe>
2020-03-15 09:57:39 -0500

    Agregando nueva demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.*;
 import com.google.api.services.classroom.model.*;
 import com.google.api.services.admin.directory.model.*;
+import javafx.util.Pair;
 
 public class Main {
 
@@ -18,7 +19,7 @@ public class Main {
         return ow.writeValueAsString(objeto);
     }
 
-    private static void demo(Classroom servicioClase) throws IOException{
+    private static void demoClase(Classroom servicioClase) throws IOException{
         List<String> nombresClases = new ArrayList<>();
         List<String> nombresTopicos= new ArrayList<>();
         List<String> nombresTareas = new ArrayList<>();
@@ -28,9 +29,17 @@ public class Main {
         nombresTareas.add("Tarea 1");
         List<String> idClases = ClassroomJavaAPI.cargaMasiva(servicioClase,nombresClases,nombresTopicos,nombresTareas);
 
+        //ClassroomJavaAPI.borradoMasivo(servicioClase,idClases);
 
-        ClassroomJavaAPI.borradoMasivo(servicioClase,idClases);
+    }
 
+    private static void demoUsuario(Directory servicioUsuario) throws IOException{
+        List<Pair<Character,String>> listaGrupos = new ArrayList<>();
+        Pair<Character,String> par1 = new Pair<>('a', "ALUMNOS_FRANQUICIA");
+        Pair<Character,String> par2 = new Pair<>('e', "ALUMNOS_COLEGIO");
+        //listaGrupos.add()
+        GsuiteJavaAPI.crearUsuario(servicioUsuario,"a00000007@sacooliveros.edu.pe", "Diaz","Marco",listaGrupos);
+        GsuiteJavaAPI.borrarUsuario(servicioUsuario,"a00000007@sacooliveros.edu.pe" );
     }
 
     public static void main(String... args) throws IOException, GeneralSecurityException {
@@ -38,7 +47,10 @@ public class Main {
         Classroom servicioClase = ClassroomJavaAPI.obtenerServicio();
         Directory servicioUsuario = GsuiteJavaAPI.obtenerServicio();
 
-        demo(servicioClase);
+        //demoClase(servicioClase);
+        //demoUsuario(servicioUsuario);
+
+
 
     }
 }
