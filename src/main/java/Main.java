50b36430e5234597aa795a4adc6481d1a7b0980c
The-Blitz <a20112387@pucp.pe>
2020-03-15 04:57:19 -0500

    Metodo para obtener objetos como JSON

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,25 @@ import com.google.api.services.admin.directory.Directory;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.google.api.services.classroom.model.*;
+import com.google.api.services.admin.directory.model.*;
+
 public class Main {
 
-    public static void main(String... args) throws IOException, GeneralSecurityException {
-        ClassroomJavaAPI classroomAPI = new ClassroomJavaAPI();
-        GsuiteJavaAPI gsuiteAPI = new GsuiteJavaAPI();
+    private static String objectToJson(Object objeto) throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        return ow.writeValueAsString(objeto);
+    }
 
-        Classroom servicioClassroom = classroomAPI.obtenerServicio();
-        Directory servicioUsuario = gsuiteAPI.obtenerServicio();
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+
+        Classroom servicioClassroom = ClassroomJavaAPI.obtenerServicio();
+        Directory servicioUsuario = GsuiteJavaAPI.obtenerServicio();
+
         //TODO: Hacer demo con las clases
 
     }
